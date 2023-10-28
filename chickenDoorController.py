09998e4b879eb5ef84d3c97e11cc2991dcaddf60
Louis Reed <mail@louisreed.co.uk>
2023-10-28 21:10:21 +0100

    Refactored-codebase

--- chickenDoorController.py
+++ chickenDoorController.py
@@ -1,233 +1,184 @@
-"""
-chickenDoorController.py
+# chickenDoorController.py
 
-This script controls the opening and closing of a chicken coop door.
-It uses a Raspberry Pi's GPIO pins to control a motor, which opens and closes the door.
-The door is scheduled to open at 6:00 and close at 20:00 every day.
-The script also listens for user input to manually open or close the door.
-"""
+# Imports
 import logging
-from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
-from datetime import datetime
+import os
+import time
 import schedule
 from threading import Thread
-import time
+from datetime import datetime
 import RPi.GPIO as GPIO
+from telegram import Update, Bot
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 from flask import Flask, jsonify, render_template
 from dotenv import load_dotenv
-import os
-import sys
-import select
 
-# Initialize logging
+
+# === Initialization ===
+
+# Initialize Logging
 logging.basicConfig(filename='chicken.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Load environment variables from .env file
+# Load Environment Variables
 load_dotenv()
-
-# Get Telegram API token
 TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
 TARGET_CHAT_ID = os.getenv('TARGET_CHAT_ID')
-
-# Log the initialization
 logger.info("Initialized Telegram bot")
 
-# Disable warnings
-GPIO.setwarnings(False)
-
 # Initialize GPIO
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup([3, 5, 7], GPIO.OUT)
 
 # Initialize PWM
 pwm = GPIO.PWM(7, 100)
 pwm.start(0)
-logger.info("pwm initialised")
+logger.info("PWM initialized")
 
-# Initialize Flask app
+# Initialize Flask
 app = Flask(__name__)
 
-# Initialize your door status
+# Initialize Door Status
 door_status = "Closed"
+progress = 0  # Door opening/closing progress
 
-# Initialize door opening/closing progress
-progress = 0
 
-# Custom logging
+# === Helper Functions ===
+
 def log_message(message):
+    """Logs a message with a timestamp to the console and a file."""
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_message = f"{current_time} - {message}"
-    
-    # Print to console
     print(formatted_message)
-    
-    # Write to file
     with open("door_log.txt", "a") as log_file:
         log_file.write(formatted_message + "\n")
 
 
-# Function to ease in and out motor speed
 def ease_motor(direction, duration):
+    """Eases the motor speed in and out over a given duration."""
     global progress
     GPIO.output(3, direction)
     GPIO.output(5, not direction)
-    
-    # Gradually increase the motor speed from 0 to 100
-    for duty in range(0, 101, 5):
+    for duty in range(0, 101, 5):  # Increase speed
         pwm.ChangeDutyCycle(duty)
         progress = duty
         time.sleep(0.1)
-    
     time.sleep(duration - 0.8)
-    
-    # Gradually decrease the motor speed from 100 to 0
-    for duty in range(100, -1, -5):
+    for duty in range(100, -1, -5):  # Decrease speed
         pwm.ChangeDutyCycle(duty)
         progress = 100 - duty
         time.sleep(0.1)
-    
-    GPIO.output(7, False)
 
 
-# Function to open and close the door
+# === Door Control Functions ===
+
 def open_door():
+    """Opens the chicken coop door."""
     global door_status
     ease_motor(True, 10)
-    log_message(f"Door opened")
-    door_status = "opened"
+    log_message("Door opened")
+    door_status = "Opened"
+
 
 def close_door():
+    """Closes the chicken coop door."""
     global door_status
     ease_motor(False, 10)
-    log_message(f"Door closed")
-    door_status = "closed"
-    
-    
-    
-def send_message(context, text):
-    context.bot.send_message(chat_id=TARGET_CHAT_ID, text=text)
-    
-# Error handler for the Telegram bot
-async def error_handler(update: Update, context: CallbackContext):
-    logger.error(f"Error handling update {update} - context: {context.error}")
+    log_message("Door closed")
+    door_status = "Closed"
 
-# Telegram command to open door (now asynchronous)
+
+# === Telegram Bot Commands ===
+
 async def tg_open_door(update: Update, context: CallbackContext):
-    open_door()  # Assuming open_door is not asynchronous
+    """Telegram command to open the door."""
+    open_door()
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Door opened.")
 
-# Telegram command to close door (now asynchronous)
+
 async def tg_close_door(update: Update, context: CallbackContext):
-    close_door()  # Assuming close_door is not asynchronous
+    """Telegram command to close the door."""
+    close_door()
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Door closed.")
 
-# Telegram command to check door status (now asynchronous)
+
 async def tg_door_status(update: Update, context: CallbackContext):
+    """Telegram command to check the door status."""
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"The door is currently {door_status}.")
 
-# Telegram command to check if the system is working (now asynchronous)
-async def tg_ping(update: Update, context: CallbackContext):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="The system is up and running.")
+
+async def error_handler(update: Update, context: CallbackContext):
+    """Handles errors for the Telegram bot."""
+    logger.error(f"Error handling update {update} - context: {context.error}")
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(TELEGRAM_API_TOKEN).build()
-    
-    # Add handlers
-    application.add_handler(CommandHandler('open', tg_open_door))
-    application.add_handler(CommandHandler('close', tg_close_door))
-    application.add_handler(CommandHandler('status', tg_door_status))
-    application.add_handler(CommandHandler('ping', tg_ping))
+# === Flask API Endpoints ===
 
-    # Add error handler
-    application.add_error_handler(error_handler)
-    
-    # Start the Bot
-    application.run_polling()
-    logger.info("Bot started")
-    
-    # Send a startup message using a synchronous bot
-    bot = Bot(token=TELEGRAM_API_TOKEN)
-    bot.send_message(chat_id=TARGET_CHAT_ID, text="The system is up and running.")
-
-
-# Function to listen for user input    
 @app.route('/api/open_door', methods=['POST'])
 def api_open_door():
-    global door_status
-    ease_motor(True, 14)
-    log_message("Door opened")
-    door_status = "Open"
+    """API endpoint to open the door."""
+    open_door()
     return jsonify(status='success')
+
 
 @app.route('/api/close_door', methods=['POST'])
 def api_close_door():
-    global door_status
-    ease_motor(False, 14)
-    log_message("Door closed")
-    door_status = "Closed"
+    """API endpoint to close the door."""
+    close_door()
     return jsonify(status='success')
 
 
-# Function to get door status
 @app.route('/api/door_status', methods=['GET'])
 def api_door_status():
-    global door_status
+    """API endpoint to get the door status."""
     return jsonify(status=door_status)
 
 
-# Function to get door opening/closing progress
 @app.route('/api/progress', methods=['GET'])
 def api_progress():
-    global progress
+    """API endpoint to get the door opening/closing progress."""
     return jsonify(progress=progress)
 
 
-# Landing page
 @app.route('/')
 def index():
+    """Landing page."""
     return render_template('index.html')
 
 
-# Schedule tasks
-schedule.every().day.at("06:00").do(open_door)
-schedule.every().day.at("20:00").do(close_door)
+# === Main Program ===
 
+if __name__ == '__main__':
+    # Telegram Bot Setup
+    application = ApplicationBuilder().token(TELEGRAM_API_TOKEN).build()
+    application.add_handler(CommandHandler('open', tg_open_door))
+    application.add_handler(CommandHandler('close', tg_close_door))
+    application.add_handler(CommandHandler('status', tg_door_status))
+    application.add_error_handler(error_handler)
+    application.run_polling()
+    logger.info("Bot started")
 
-# Main loop for Flask app
-def flask_thread():
-    app.run(host='0.0.0.0', port=5000)
+    # Scheduler Setup
+    schedule.every().day.at("06:00").do(open_door)
+    schedule.every().day.at("20:00").do(close_door)
 
+    # Start Flask Thread
+    flask_thread = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
+    flask_thread.daemon = True
+    flask_thread.start()
 
-# Main loop for scheduler
-def scheduler_thread():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # Start Scheduler Thread
+    scheduler_thread = Thread(target=scheduler_thread)
+    scheduler_thread.daemon = True
+    scheduler_thread.start()
 
-
-# Start Flask thread
-flask_thread = Thread(target=flask_thread)
-flask_thread.daemon = True
-flask_thread.start()
-
-
-# Start scheduler thread
-scheduler_thread = Thread(target=scheduler_thread)
-scheduler_thread.daemon = True
-scheduler_thread.start()
-
-
-# Main loop
-try:
-    logger.info("Entering main loop")
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    log_message(f"Exiting...")
-    pwm.stop()
-    GPIO.cleanup()
+    # Main Loop
+    try:
+        logger.info("Entering main loop")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        log_message("Exiting...")
+        pwm.stop()
+        GPIO.cleanup()
